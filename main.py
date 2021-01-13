242976a28574ed1e47267fb7150fff6cadf4323f
Ramona Saintandre <codinggrandma@gmail.com>
2021-01-13 12:21:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+
+# # print ('Hello World ')
+
+# # def hello ():
+# #   print ("Hello from func")
+
+# # hello() 
+
+# # name = input('enter your name : ')
+# # print ('Hello', name)
+# # age = input('Enter your age :')
+# # age = int(age)
+# # print ('You are',age,"years old")
+
+# ''' this is multi line comment 
+# this is how it works '''
+
+# # if (5>2):
+# #   print ('5 is greater')
+# #   print ('2 is lesser')
+
+# # x = 10
+# # y = 'Ramona'
+# # print (type(x))
+# # print (type(y))
+
+# # x = str(5)
+# # y = int(5)
+# # z = float(5)
+
+# # print (z)
+
+# # x = 5
+# # X = 10 
+
+# # print (x)
+# # print (X)
+
+# # x, y, z = 'Orange', 'banana', 'Cherry'
+# # print (x)
+# # print (y)
+# # print (z)
+
+# # x = y = z = 'Orange'
+
+# # print (x)
+# # print (y)
+# # print (z)
+
+# # x = 'python'
+# # y = ' is great '
+
+# # print (x+y)
+
+# myvar = 10
+
+# def myfunc():
+#   myvar = 15
+  
+#   print (myvar)
+
+# print(myvar)
+# myfunc()
+
+x = 
